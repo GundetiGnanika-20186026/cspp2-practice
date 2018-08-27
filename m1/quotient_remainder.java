9f8a91e0a9f99d05dcb0c7346045b658a88d17cd
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-27 17:49:02 +0530

    finding quotient and rem

---
+++ m1/quotient_remainder.java
@@ -0,0 +1,10 @@
+public class quotient_remainder {
+	public static void main(final String[] args) {
+		int a = 20;
+		int b = 3;
+		int qnt = a / b;
+		int rem = a % b;
+		System.out.println("quotient : "+qnt+" reminder : "+rem);
+
+	}
+}
