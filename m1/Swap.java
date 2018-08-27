5512497622e56fdf36c084b06e341d7d20a4d1b6
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-27 17:52:43 +0530

    swapping mannually

---
+++ m1/Swap.java
@@ -0,0 +1,12 @@
+/* swapping two numbers */
+public class Swap {
+	/* main function */
+	public static void main(final String[] args) {
+		int a = 2;
+		int b = 3;
+		int c = a;
+		a = b;
+		b = c;
+		System.out.println("the swapped values are : "+a+" "+b);
+	}
+}
