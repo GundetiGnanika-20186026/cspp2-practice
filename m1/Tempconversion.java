6e82253c5edd938093345bd9bde5db0665627aaf
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-27 18:13:48 +0530

    tempconversion

---
+++ m1/Tempconversion.java
@@ -0,0 +1,7 @@
+public class Tempconversion {
+	public static void main(final String[] args) {
+	double celcious = 53.3;
+	double forenheit = (celcious * 1.8 + 32);
+	System.out.println("forenheit temp of celcious "+celcious+"degrees centigrade is : "+forenheit);
+	}
+}
