122f801d60fd334d887f90ce2421a4de000839d8
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-28 17:25:59 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -19,7 +19,7 @@ final class Solution {
 	/*
 	Need to write the rootsOfQuadraticEquation function and print the output.
 	*/
-	static double rootsOfQuadraticEquation(int a, int b, int c) {
+	static double rootsOfQuadraticEquation(final int a, final int b, final int c) {
 		double value = (b * b) - (4 * a * c);
 		double r1 = (-b + Math.sqrt(value)) / (2 * a);
         double r2 = (-b - Math.sqrt(value)) / (2 * a);
