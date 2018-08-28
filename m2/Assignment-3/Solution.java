59824f29305bd1fbc1e64fac9a8a3c163b685d66
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-28 19:06:39 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m2/Assignment-3/Solution.java
+++ m2/Assignment-3/Solution.java
@@ -16,7 +16,7 @@ public class Solution {
 	Need to write the power function and print the output.
 	*/
 	static long power(int base, int exponent) {
-		if (exponent == 1) return base;
+		if (exponent == 0) return 1;
 		else return base * power(base, exponent - 1); 
 
 	}
