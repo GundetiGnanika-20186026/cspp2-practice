be722866b60caa4ec294592c1f7fc7df8dd82be9
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-28 14:40:43 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ m2/Assignment-2/Assignment-2/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		System.out.println("000");
+		// rootsOfQuadraticEquation(a, b, c);
+	}
+	/*
+	Need to write the rootsOfQuadraticEquation function and print the output.
+	*/
+}
