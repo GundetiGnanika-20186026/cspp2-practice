262f76b9a1d9fa7f1bef1b58e5de91e0ec565eef
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-28 17:28:33 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -2,28 +2,28 @@ import java.util.Scanner;
 import java.lang.Math;
 /** code to find quadratic. **/
 final class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	private Solution() { }
-	/**
-	   * @param args this is the constructor.
-	**/
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		roots(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation function.
-	*/
-	static double roots(final int a, final int b, final int c) {
-		double value = (b * b) - (4 * a * c);
-		double r1 = (-b + Math.sqrt(value)) / (2 * a);
+    /*
+    Do not modify this main function.
+    */
+    private Solution() { }
+    /**
+       * @param args this is the constructor.
+    **/
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        roots(a, b, c);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation function.
+    */
+    static double roots(final int a, final int b, final int c) {
+        double value = (b * b) - (4 * a * c);
+        double r1 = (-b + Math.sqrt(value)) / (2 * a);
         double r2 = (-b - Math.sqrt(value)) / (2 * a);
         System.out.println(r1 + " " + r2);
         return 0;
-	} 
+    } 
 }
